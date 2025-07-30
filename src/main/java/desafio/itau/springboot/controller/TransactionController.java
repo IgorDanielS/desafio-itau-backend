7bce8501175496aee1045046dc9617ae72a5a7bc
IgorDanielS <igordaniel1903@gmail.com>
2025-07-30 10:52:01 -0300

    delete Transaction

--- src/main/java/desafio/itau/springboot/controller/TransactionController.java
+++ src/main/java/desafio/itau/springboot/controller/TransactionController.java
@@ -5,12 +5,8 @@ import desafio.itau.springboot.model.Transaction;
 import desafio.itau.springboot.services.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
 
@@ -32,5 +28,11 @@ public class TransactionController {
         transactionService.addTransaction(new Transaction(request.getValor(), request.getDataHora()));
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> clearTransaction(){
+        transactionService.clearTransactions();
+        return ResponseEntity.ok().build();
     }
 }
